afd5b2ee979befb3c61059b079086a898352e0f8
MounderBoutebba <mounderboutebba@outlook.fr>
2019-10-14 07:08:37 +0100

    added simple styling using nebular

--- TicTacToe/src/app/app.module.ts
+++ TicTacToe/src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SquareComponent } from './square/square.component';
-import { BoardComponent } from './board/board.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SquareComponent } from "./square/square.component";
+import { BoardComponent } from "./board/board.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SquareComponent,
-    BoardComponent
-  ],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: "dark" }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
